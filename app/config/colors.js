801d331659ddfee3b4feb97e845e7651658bdc04
Invenst PhoneFive <invenstphonefive@dhcp020-197-021.wireless.buffalo.edu>
2021-10-27 15:21:23 -0400

    First commit

---
+++ app/config/colors.js
@@ -0,0 +1,9 @@
+export default {
+    primary: '#000',
+    secondary: '#fff',
+    grey: '#808080',
+    dullblack: '#3F3F3F',
+    gold: '#FFC107',
+    lightgrey: '#D3D3D3',
+    silver: '#C0C0C0'
+}
