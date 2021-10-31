e6877ded0470b0b8829f01b1dc0e71b5e132b6b8
Invenst PhoneFive <invenstphonefive@Invensts-MBP.lan>
2021-10-31 17:07:09 -0400

    Improving map features, geocoding, user location and initial region

--- app/components/athlete/AthleteMapView.js
+++ app/components/athlete/AthleteMapView.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import colors from '../../config/colors';
 
-function AthleteMapView(props) {
+function AthleteMapView({markers, selectedMarker, userLocation}) {
+    const [region, setRegion] = useState({
+        latitude: 39.129065,
+        longitude: -95.141081,
+        latitudeDelta: 55,
+        longitudeDelta: 55
+      });
+
+      useEffect(() => {
+        setRegion({
+            latitude: userLocation? userLocation.coords.latitude: 39.129065,
+            longitude: userLocation? userLocation.coords.longitude: -95.141081,
+            latitudeDelta: userLocation? 0.1 : 55,
+            longitudeDelta: userLocation? 0.1 : 55});
+      }, [markers]);
+
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} />
+            <MapView showsUserLocation style={styles.map} 
+            region={region}
+            onRegionChangeComplete={region => setRegion(region)}>
+            {markers && markers.map((marker, index) => (
+               <Marker 
+               key={index}
+               coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}>
+                   <FontAwesome5 name="map-marker-alt" size={40} color={colors.primary} />
+               </Marker>
+             ))}
+            </MapView>
         </View>
     );
 }
--- app/screens/athlete/AthleteBookNow.js
+++ app/screens/athlete/AthleteBookNow.js
@@ -1,14 +1,102 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
+
+import * as Location from 'expo-location';
 
 import AthleteBookNowCard from '../../components/athlete/AthleteBookNowCard';
 import AthleteMapView from '../../components/athlete/AthleteMapView';
 
-console.log('in Book Now')
+const therapists = [
+    {
+        email: "",
+        fname: "Roger",
+        lname: "Apples",
+        mobile: "7164578324",
+        address: {
+            street: "112 Callodine Avenue",
+            aptNum: "3",
+            state: "New York",
+            zipcode: "14214",
+            city: "Buffalo"
+
+        },
+        availabilityStatus: true,
+        enabledStatus: 1,
+        avgRating: 4.2,
+        therapistId: 23
+
+    },
+    {
+        email: "",
+        fname: "Joseph",
+        lname: "Oranges",
+        mobile: "7164578324",
+        address: {
+            street: "12 Robin Road",
+            aptNum: "Upper",
+            state: "New York",
+            zipcode: "14214",
+            city: "Buffalo"
+
+        },
+        availabilityStatus: true,
+        enabledStatus: 1,
+        avgRating: 3.45,
+        therapistId: 23
+
+    },
+    {
+        email: "",
+        fname: "Rachel",
+        lname: "Green",
+        mobile: "7164578324",
+        address: {
+            street: "34 Spruce Road",
+            aptNum: "3",
+            state: "New York",
+            zipcode: "14214",
+            city: "Buffalo"
+
+        },
+        availabilityStatus: true,
+        enabledStatus: 1,
+        avgRating: 4.6,
+        therapistId: 23
+
+    },
+]
+
 function AthleteBookNow(props) {
+    const [selectedTherapist, setSelectedTherapist] = useState(therapists[0]);
+    const [location, setLocation] = useState(null);
+    const [markers, setMarkers] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+          let { status } = await Location.requestForegroundPermissionsAsync();
+        //   if (status !== 'granted') {
+        //     //setErrorMsg('Permission to access location was denied');
+        //   Something to think about: Location.getLastKnownPositionAsync(options)
+        //     return;
+        //   }
+    
+          let location = await Location.getCurrentPositionAsync({});
+          setLocation(location);
+
+          let promises = therapists.map(async therapist => {
+            let locPromise = await Location.geocodeAsync(therapist.address.street + ' ' + therapist.address.city + ' ' + therapist.address.state);
+            return locPromise[0];
+          })
+        
+          let results = await Promise.all(promises)
+          setMarkers(results);
+          //console.log(results);
+        })();
+      }, []);
+
     return (
-        <View style={{flex:1,marginBottom: 20,}}>
-            <AthleteMapView/>
+        <View style={{flex:1, marginBottom: 20,}}>
+            <AthleteMapView markers={markers} selectedTherapist={selectedTherapist} userLocation={location}/>
             <AthleteBookNowCard></AthleteBookNowCard>
         </View>
     );
