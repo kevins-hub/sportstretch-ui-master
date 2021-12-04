7b77ea843a24d7899778fc46a29cdb4658063006
digant0705 <digant0705@gmail.com>
2021-12-04 07:12:06 -0500

    updating the register api for therapist

--- app/api/register.js
+++ app/api/register.js
@@ -2,10 +2,22 @@ import apiClient from "./client";
 
 const endpoint = '/register';
 
+<<<<<<< Updated upstream
 const registerAthlete = (athlete) => {
     return apiClient.post(endpoint + '/athlete', athlete);
 }
 
-export default {
-    registerAthlete,
+=======
+>>>>>>> Stashed changes
+const registerTherapist = (therapistObj) => {
+    return apiClient.post(endpoint + "/therapist", therapistObj);
 }
+
+export default {
+<<<<<<< Updated upstream
+    registerAthlete,
+=======
+>>>>>>> Stashed changes
+    registerTherapist
+}
+
