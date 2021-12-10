ef46e4472de475d1a8385e713623f034256e2328
Invenst PhoneFive <invenstphonefive@Invensts-MBP.lan>
2021-12-09 22:35:29 -0500

    Disabling restore login session for demo

--- App.js
+++ App.js
@@ -12,13 +12,13 @@ export default function App() {
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState(false);
 
-  const restoreUser = async () => {
-    const user = await authStorage.getUser();
-    if (user) setUser(user);
-  }
+  // const restoreUser = async () => {
+  //   const user = await authStorage.getUser();
+  //   if (user) setUser(user);
+  // }
 
-  if (!isReady)
-    return <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} onError={console.warn} />
+  // if (!isReady)
+  //   return <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} onError={console.warn} />
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
