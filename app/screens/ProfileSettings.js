bb5b554d899bdf6991ee05281f7b8c9e8f5a5bb2
Kevin Liu <kevinliu@Kevins-MBP.lan>
2023-09-23 13:17:16 -0700

    profile page base commit

--- app/screens/ProfileSettings.js
+++ app/screens/ProfileSettings.js
@@ -1,30 +1,92 @@
 import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import LogOutButton from "../components/shared/LogOutButton";
 import ChangePasswordButton from "../components/shared/ChangePasswordButton";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+// import FontAwesome from '@expo/vector-icons';
+import colors from "../config/colors";
 
 function ProfileSettings({ route }) {
 
   const { user } = route.params;
-  console.warn("user = ", user);
-  console.warn("user.userObj = ", user.userObj);
+  // console.warn("user = ", user);
+  // console.warn("user.userObj = ", user.userObj);
 
   return (
     <>
-        <View>
-            <Text>Profile settings</Text>
-            <Text>Account Type: {user.role}</Text>
-            <Text>Subscription: Free</Text>
-            <Text>Name: {user.userObj.first_name} {user.userObj.last_name}</Text>
-            <Text>Phone number: {user.userObj.mobile}</Text>
-            <Text>Email: {user.userObj.email}</Text>
+        <View style={styles.container}>
+            {/* <Text>Profile settings</Text> */}
+            {/* <FontAwesome name="user-circle" size={73} color="white" /> */}
+            <View style={styles.iconNameContainer}>
+              <MaterialCommunityIcons style={styles.accountIcon} name="account-circle" size={73} color={colors.primary}/>
+              <Text>{user.userObj.first_name} {user.userObj.last_name}</Text>
+            </View>
+            <ScrollView contentContainerStyle={styles.profileSummaryContainer}>
+              <View style={styles.propContainer}>
+                <Text style={styles.propLabel}>Account Type:</Text>
+                <Text>{user.role === 'therapist' ? 'Recovery Specialist' : user.role.charAt(0).toUpperCase() + user.role.slice(1)}</Text>
+              </View>
+              <View style={styles.propContainer}>
+                <Text style={styles.propLabel}>Subscription:</Text>
+                <Text>Free</Text>
+              </View>
+              <View style={styles.propContainer}>
+                <Text style={styles.propLabel}>Location:</Text>
+                <Text>California</Text>
+              </View>
+              <View style={styles.propContainer}>
+                <Text style={styles.propLabel}>Phone number:</Text>
+                <Text>{user.userObj.mobile}</Text>
+              </View>
+              <View style={styles.propContainer}>
+                <Text style={styles.propLabel}>Email: </Text>
+                <Text>placeholder@email.com</Text>
+              </View>
+              <View style={styles.propContainer}>
+                <Text style={styles.propLabel}>Payment Method: </Text>
+                <Text>XXXX-XXXX-XXXX-1234</Text>
+              </View>
+            </ScrollView>
 
-            <Text></Text>
+            <LogOutButton/>
         </View>
-        <ChangePasswordButton/>
-        <LogOutButton/>
+
     </>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      height: '100%',
+      alignItems: 'center',
+      flex: 1,
+    },
+    iconNameContainer: {
+      width: '100%',
+      alignItems: 'center',
+    },
+    profileSummaryContainer: {
+      width: '100%',
+      marginTop: '4%',
+      paddingLeft: '2%',
+      paddingRight: '2%',
+      alignItems: 'left',
+    },
+    propContainer: {
+      marginBottom: 10
+    },
+    propLabel: {
+      fontWeight: 'bold',
+      marginBottom: 5,
+    },
+    accountIcon: {
+      marginTop: '5%'
+    },
+
+  });
 
 export default ProfileSettings;
