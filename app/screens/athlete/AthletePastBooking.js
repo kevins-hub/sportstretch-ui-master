c6012a7d7189fc933f2170d88518cf43b46590ea
Kevin Liu <kevinliu@Kevins-MBP-2.lan>
2024-05-08 19:06:32 -0700

    add default states for athlete past and upcoming booking

--- app/screens/athlete/AthletePastBooking.js
+++ app/screens/athlete/AthletePastBooking.js
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, Text, StyleSheet } from "react-native";
 
 import AthletePastCard from "../../components/athlete/AthletePastCard";
 import bookingsApi from "../../api/bookings";
 import AuthContext from "../../auth/context";
+import { styleProps } from "react-native-web/dist/cjs/modules/forwardedProps";
 
 function AthletePastBooking(props) {
   const [pastBookings, setPastBookings] = useState([]);
@@ -30,21 +31,40 @@ function AthletePastBooking(props) {
   };
 
   return (
-    <FlatList
-      data={pastBookings.sort((a, b) => a.bookings_id < b.bookings_id)}
-      keyExtractor={(message) => message.bookings_id.toString()}
-      renderItem={({ item }) => (
-        <AthletePastCard
-          BookingMonth={item.booking_month}
-          BookingDay={item.booking_day}
-          fname={item.first_name}
-          bookingId={item.bookings_id}
-          therapistId={item.therapist_id}
-          starRating={item.starrating}
+    <>
+      {pastBookings.length === 0 && (
+        <Text style={styles.defaultText}>
+          Your completed appointments will show up here!
+        </Text>
+      )}
+      {pastBookings.length > 0 && (
+        <FlatList
+          data={pastBookings.sort((a, b) => a.bookings_id < b.bookings_id)}
+          keyExtractor={(message) => message.bookings_id.toString()}
+          renderItem={({ item }) => (
+            <AthletePastCard
+              BookingMonth={item.booking_month}
+              BookingDay={item.booking_day}
+              fname={item.first_name}
+              bookingId={item.bookings_id}
+              therapistId={item.therapist_id}
+              starRating={item.starrating}
+            />
+          )}
         />
       )}
-    />
+    </>
   );
 }
 
+const styles = StyleSheet.create({
+  defaultText: {
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: "auto",
+    marginBottom: "auto",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+});
 export default AthletePastBooking;
