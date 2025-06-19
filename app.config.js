ca17b418a269ccffd148ba6ea0fe9dafb710166a
Kevin Liu <kevinliu428@gmail.com>
2025-06-19 12:09:40 -0700

    add permission to app config, and check for rc custoemr ID before calling rc login

--- app.config.js
+++ app.config.js
@@ -22,6 +22,7 @@ export default {
         NSLocationWhenInUseUsageDescription:
           "This app uses the location to provide the client with a list of nearby service providers.",
         NSCameraUsageDescription: "This app uses the camera to let therapists upload profile photos.",
+        NSPhotoLibraryUsageDescription: "This app uses the photo library to let therapists upload profile photos.",
       },
     },
     android: {
