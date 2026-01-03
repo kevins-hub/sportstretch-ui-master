e96fe94b6643c0a5d83f89a8e848b145c6d5b081
Kevin Liu <kevinliu428@gmail.com>
2026-01-03 13:03:29 -0800

    build 61

--- app.config.js
+++ app.config.js
@@ -15,7 +15,7 @@ export default {
     },
     assetBundlePatterns: ["**/*"],
     ios: {
-      buildNumber: "60",
+      buildNumber: "61",
       bundleIdentifier: "com.sportstretchusa.sportstretch",
       supportsTablet: true,
       infoPlist: {
